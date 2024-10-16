f48054def69a178df700ad32e1d0c6278de3b16c
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2024-10-16 09:40:17 +0200

    commentaire

--- src/main/java/org/github/ahmed/Main.java
+++ src/main/java/org/github/ahmed/Main.java
@@ -1,4 +1,4 @@
-package org.github.bastide;
+package org.github.ahmed;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello!");
 
 
     }
